f1fc08ad6bad68584c58c8ebc93a4448cb01a010
Milena Yamamoto <milymanamy3@gmail.com>
2025-01-03 09:20:04 +0900

    Exercises from 3.13 to 3.15 are done

--- Chapter_3/exercise3.py
+++ Chapter_3/exercise3.py
@@ -43,4 +43,26 @@
 # # se tiver só 1 dígito ou nada, o 0 será preenchido nos lugares vazios
 # print(f"Seu tempo para chegar é de {horas:02}:{minutos:02}:{segundos:02}")
 
+3.13
+# C = float(input("Digite a temperatura em °C:"))
+# F = (9 * C / 5) + 32
+# print(f"A temperatura de {C:1.2f} °C é {F:1.2f} °F")
 
+3.14
+# km = int(input("Quantos km percorridos: "))
+# dias = int(input("Quantos dias: "))
+# preço_por_dia = 60
+# preço_por_km = 0.15
+# preco = km * preço_por_km + dias * preço_por_dia
+# if dias == 1:
+#     print(f"Com a distância de {km:1} km e de apenas {dias:1} dia, o preço a pagar é de R${preco:1.2f}")
+# else:
+#     print(f"Com a distância de {km:1} km e de {dias:1} dias, o preço a pagar é de R${preco:1.2f}")
+
+3.15
+# cigarros = int(input("Quantos cigarros por dia: "))
+# anos = float(input("Quantos anos fumando: "))
+# redução_em_min = anos*365*cigarros*10
+# # Um dia tem 24 x 60 minutos
+# redução_em_dias = redução_em_min/(24*60)
+# print(f"Redução do tempo de vida é de {redução_em_dias:1.1f} dias")
